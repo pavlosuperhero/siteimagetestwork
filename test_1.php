e4a48a3d17481b28e23c8e0ec61ae7bcc4e86132
Pavlo <pavlo.fabuliak@gmail.com>
2017-04-21 19:33:31 +0000

    tests

--- test_1.php
+++ test_1.php
@@ -9,7 +9,7 @@
     $output = "stored in file";
 } elseif ($dest == 'db') {
     $db = new PDO('mysql:dbname=testdb;host=127.0.0.1', 'user', 'pass');
-    $db->exec("INSERT INTO storage (value) VALUES ('{$result}');");
+    $db->exec("INSERT INTO storage (result) VALUES ('$result')");
     $output = "stored in db";
 }
 
